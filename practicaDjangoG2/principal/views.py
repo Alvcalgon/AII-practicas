77df406bb18d90271f8b58ac4a2341ddbf306a9d
alvcalgon <alvcalgon	@alum.us.es>
2018-12-10 12:02:58 +0100

    plantillas para los formularios

--- practicaDjangoG2/principal/views.py
+++ practicaDjangoG2/principal/views.py
@@ -1,27 +1,97 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.db.models import Max
-from principal.models import Sucursales, Movimientos
+
+from principal.models import Sucursal, Movimiento
+from principal.forms import BancoForm, SucursalForm, UsuarioForm, CuentaForm, MovimientoForm
 
 # Create your views here.
 def index(request):
     return HttpResponse("Esta es la pagina indice de la app Principal.")
 
 
-def sucursales(request,id_banco):
-    sucursales=Sucursales.objects.all(Sucursales,fk_banco=id_banco)
+def sucursales(request):
+    sucursales= Sucursal.objects.order_by('banco')
     
-    return render(request,'sucursales.html', {'datos':sucursales,'MEDIA_URL': settings.MEDIA_URL})
+    return render(request, 'sucursales.html', {'datos':sucursales,'MEDIA_URL': settings.MEDIA_URL})
 
 
 def usuarios(request):
-    usuario=Movimientos.objects.all().aggregate(Max('euros'))
+    usuario= Movimiento.objects.all().aggregate(Max('euros'))
     
     return render(request,'usuarios.html', {'datos':usuario,'MEDIA_URL': settings.MEDIA_URL})
 
-
+"""
 def usuarios(request):
-    usuario=Movimientos.objects.filter(Sucursales=fk_banco).order_by('-euros')[:2]
+    usuario = Movimiento.objects.filter(Sucursales=fk_banco).order_by('-euros')[:2]
     
     return render(request,'usuarios.html', {'datos':usuario,'MEDIA_URL': settings.MEDIA_URL})
+"""
+
+# Formularios -------------------------------------------------------
+
+def get_banco(request):
+    if request.method == 'POST':
+        form = BancoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inicio')
+    else:
+        form = BancoForm()
+
+    return render(request, 'form_banco.html', {'form': form})
+
+
+def get_sucursal(request):
+    if request.method == 'POST':
+        form = SucursalForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inicio')
+    else:
+        form = SucursalForm()
+
+    return render(request, 'form_sucursal.html', {'form': form})
+
+
+def get_usuario(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inicio')
+    else:
+        form = UsuarioForm()
+
+    return render(request, 'form_usuario.html', {'form': form})
+
+
+def get_cuenta(request):
+    if request.method == 'POST':
+        form = CuentaForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inicio')
+    else:
+        form = CuentaForm()
+
+    return render(request, 'form_cuenta.html', {'form': form})
+
+
+def get_movimiento(request):
+    if request.method == 'POST':
+        form = MovimientoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inicio')
+    else:
+        form = MovimientoForm()
+
+    return render(request, 'form_movimiento.html', {'form': form})
+
