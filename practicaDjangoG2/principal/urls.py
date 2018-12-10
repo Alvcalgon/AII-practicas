391b819eabc340f38d94ac5ac3b180081a058683
alvcalgon <alvcalgon	@alum.us.es>
2018-12-10 12:19:15 +0100

    error sol en urls.py

--- practicaDjangoG2/principal/urls.py
+++ practicaDjangoG2/principal/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from . import views
-from principal import views
 from django.contrib import admin
 from django.urls import re_path
 from django.conf import settings
 from django.views import static
+
 from principal.views import index
+from principal import views
 
 urlpatterns = [
         path('', views.index, name = 'index'),
-        path('form/',views.form),
-        path('sucursales/', views.sucursales),
-        path('usuarios/', views.usuarios),
-        path('formMov/', views.formMov)
-]
+        path('banco/edit', views.get_banco),
+        path('sucursal/edit', views.get_sucursal),
+        path('usuario/edit', views.get_usuario),
+        path('cuenta/edit', views.get_cuenta),
+        path('movimiento/edit', views.get_movimiento),
+        path('sucursal/list', views.sucursales),
+        path('usuario/list', views.usuarios),
     ]
