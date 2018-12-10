cb9d8468060de640d9abf2d373d530255c4ec032
ferruipli <fernandoruizp97@gmail.com>
2018-12-10 11:37:33 +0100

    vistas y templates

--- practicaDjangoG2/principal/urls.py
+++ practicaDjangoG2/principal/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
 from . import views
+from principal import views
+from django.contrib import admin
+from django.urls import re_path
+from django.conf import settings
+from django.views import static
 
-url_patterns = [
+urlpatterns = [
         path('', views.index, name = 'index'),
+        path('form/',views.form),
+        path('sucursales/', views.sucursales),
+        path('usuarios/', views.usuarios),
+        path('formMov/', views.formMov)
     ]
--- practicaDjangoG2/principal/views.py
+++ practicaDjangoG2/principal/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from django.db.models import Max
+from principal.models import Sucursales, Movimientos
 
 # Create your views here.
 def index(request):
     return HttpResponse("Esta es la pagina indice de la app Principal.")
+
+
+def sucursales(request,id_banco):
+    sucursales=Sucursales.objects.all(Sucursales,fk_banco=id_banco)
+    
+    return render(request,'sucursales.html', {'datos':sucursales,'MEDIA_URL': settings.MEDIA_URL})
+
+
+def usuarios(request):
+    usuario=Movimientos.objects.all().aggregate(Max('euros'))
+    
+    return render(request,'usuarios.html', {'datos':usuario,'MEDIA_URL': settings.MEDIA_URL})
+
+
+def usuarios(request):
+    usuario=Movimientos.objects.filter(Sucursales=fk_banco).order_by('-euros')[:2]
+    
+    return render(request,'usuarios.html', {'datos':usuario,'MEDIA_URL': settings.MEDIA_URL})
