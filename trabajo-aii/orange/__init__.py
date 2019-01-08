e865452c5c535422bfd9bdc64091cfc621d76172
ferruipli <fernandoruizp97@gmail.com>
2019-01-09 00:06:07 +0100

    tarifas movil hablar y navegar

--- trabajo-aii/orange/__init__.py
+++ trabajo-aii/orange/__init__.py
@@ -10,7 +10,7 @@ import sqlite3
 def extraer_tarifas_hablar_navegar():
     f = urllib.request.urlopen("https://www.phonehouse.es/tarifas/orange/movil-contrato.html")
     s = BeautifulSoup(f, "lxml")
-    l = s.find_all("li", class_=["resultados"])
+    l = s.find_all("li", class_=["linea"])
     return l
 
 def cargar_tarifas_hablar_navegar():
@@ -20,29 +20,63 @@ def cargar_tarifas_hablar_navegar():
     
     for e in l:
         tag_nombre_tarifa = e.find("div", class_ = ["nombre-tarifa"])
-        tag_titulo_tabla = e.find_all("div", class_ = ["datos"])
-        for i in tag_titulo_tabla:
-            tag_titulo = i.find("div", class_ = ["titulo"])
-            titulo = tag_titulo.string.strip()
+
+        #tag_titulo_tabla = e.find_all("div", class_ = ["datos"])
+        #for i in tag_titulo_tabla:
+            #tag_titulo = i.find("div", class_ = ["titulo"])
+            #titulo = tag_titulo.string.strip()
             
-            
+        tag_div_minutos_tarifa = e.find_all("div", class_ = ["col_2"])
+        for i in tag_div_minutos_tarifa:
+            tag_minutos_tarifa = i.find("ul")
+            #tag_minutos_tarifa = i.find("ul")
+            minutos_tarifa = tag_minutos_tarifa.string
+            if(minutos_tarifa == None):
+                minutos_tarifa = tag_minutos_tarifa
+                
+         
+        tag_div_internet_tarifa = e.find_all("div", class_ = ["col_3"])
+        for j in tag_div_internet_tarifa:
+            tag_internet_tarifa = j.find("li")
+            internet_tarifa = tag_internet_tarifa.string.strip()
+         
+        tag_ul_promocion_tarifa = e.find_all("ul", class_= ["promo"])
+        for z in tag_ul_promocion_tarifa:
+            tag_promocion_tarifa = z.find("li")
+            promocion_tarifa = tag_promocion_tarifa.string.strip()
+             
+        tag_div_precio_tarifa = e.find_all("div", class_ = ["precio"])
+        for a in tag_div_precio_tarifa:
+            tag_precio_tarifa = a.find("strong")
+            precio_tarifa = tag_precio_tarifa.string.strip()
+             
+             
+             
         nombre_tarifa = tag_nombre_tarifa.string.strip()
+         
+        precio_tarifa = tag_precio_tarifa.string.strip()
+         
+        if(nombre_tarifa == None):
+            nombre_tarifa = ""
+        elif (minutos_tarifa == None):
+            minutos_tarifa = ""
+        elif (internet_tarifa == None):
+            internet_tarifa = ""
+        elif (promocion_tarifa == None):
+            promocion_tarifa = ""
+        elif(precio_tarifa == None):
+            precio_tarifa = ""    
+            
         
         
-        tag_enlace_imagen = e.find("img")
-        tag_descripcion = e.find("p", class_=["desc"])
-        tag_fecha = e.find("p", class_=["fec"])
-        
-        antetitulo = tag_p_antetitulo.string.strip()
-        enlace_imagen = tag_enlace_imagen['src']
-        descripcion = tag_descripcion.string.strip()
-        fecha = tag_fecha.string.strip()
+        tarifa = [nombre_tarifa, minutos_tarifa, internet_tarifa, promocion_tarifa, precio_tarifa+' €']
+        #tarifa = [nombre_tarifa]
+        lista_tarifas.append(tarifa)
         
         
-        noticia = [antetitulo, titulo, enlace_imagen, descripcion, fecha]
-        lista_noticias.append(noticia)
-        
-        
-        return (lista_noticias)
+    return (lista_tarifas)
+    
+    
+print(cargar_tarifas_hablar_navegar())
                 
     
